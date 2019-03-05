d35fb3714723834b05bfa9e26e2a440eb76a1442
Quentin PENAULT <quentinpenault@live.fr>
2019-03-05 17:12:40 +0100

    Added simple methods & unit tests for calculator

--- src/main/java/quentinetpierre/calculator/SuperCalculator1998Application.java
+++ src/main/java/quentinetpierre/calculator/SuperCalculator1998Application.java
@@ -1,10 +1,24 @@
 package quentinetpierre.calculator;
 
-import org.springframework.boot.SpringApplication;
-
 public class SuperCalculator1998Application {
 
 	public static void main(String[] args) {
+	}
+
+	public int plus(int a, int b){
+		return a+b;
+	}
+
+	public int minus(int a, int b){
+		return a-b;
+	}
+
+	public int multiply(int a, int b){
+		return a*b;
+	}
+
+	public int divide(int a, int b){
+		return a/b;
 	}
 
 }
--- src/test/java/quentinetpierre/calculator/SuperCalculator1998ApplicationTests.java
+++ src/test/java/quentinetpierre/calculator/SuperCalculator1998ApplicationTests.java
@@ -1,13 +1,35 @@
 package quentinetpierre.calculator;
 
 import org.junit.Test;
-import static org.testng.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SuperCalculator1998ApplicationTests {
 
+	static SuperCalculator1998Application calculator;
+
+	public static void beforeClass(){
+		calculator = new SuperCalculator1998Application();
+	}
+
 	@Test
-	public void test(){
-		assertTrue(true);
+	public void plus(){
+		assertEquals(calculator.plus(1,2), 3);
+	}
+
+	@Test
+	public void minus(){
+		assertEquals(calculator.minus(3,2), 1);
+	}
+
+	@Test
+	public void multiply(){
+		assertEquals(calculator.multiply(2,2), 4);
+	}
+
+	@Test
+	public void devide(){
+		assertEquals(calculator.divide(2,1), 1);
 	}
 
 }
