5a043b79754555ed547e39fcf1930d64d21fd917
Quentin PENAULT <quentinpenault@live.fr>
2019-03-05 16:45:17 +0100

    Initial commit

---
+++ src/main/java/quentinetpierre/calculator/SuperCalculator1998Application.java
@@ -0,0 +1,10 @@
+package quentinetpierre.calculator;
+
+import org.springframework.boot.SpringApplication;
+
+public class SuperCalculator1998Application {
+
+	public static void main(String[] args) {
+	}
+
+}
---
+++ src/test/java/quentinetpierre/calculator/SuperCalculator1998ApplicationTests.java
@@ -0,0 +1,14 @@
+package quentinetpierre.calculator;
+
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class SuperCalculator1998ApplicationTests {
+
+	@Test
+	public void test(){
+		Assert.assertTrue(true);
+	}
+
+}
