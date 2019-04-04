81dd98b8c44616984a178604aeb5dec8ecf65fee
Pierre LEZAN <1pierrelezan@gmail.com>
2019-04-04 16:10:15 +0200

    Tests avancés pour la Divisions

--- src/test/java/quentinetpierre/calculator/DivisionTest.java
+++ src/test/java/quentinetpierre/calculator/DivisionTest.java
@@ -2,6 +2,9 @@ package quentinetpierre.calculator;
 
 import org.junit.Before;
 import org.junit.Test;
+import quentinetpierre.calculator.exceptions.OutOfBoundException;
+
+import java.lang.reflect.Method;
 
 import static org.junit.Assert.*;
 
@@ -21,6 +24,23 @@ public class DivisionTest {
     }
 
 
+    // TEST METHOD EXISTS
+    @Test
+    public void divideMethodExists(){
+        Boolean b = false;
+
+        for (Method m : calculator.getClass().getMethods() ) {
+            if(m.getName().equals("divide")){
+                if( m.getParameterCount() == 2
+                        && m.getParameterTypes()[0] == Integer.class
+                        && m.getParameterTypes()[1] == Integer.class
+                ) b = true;
+            }
+        }
+
+        assertTrue(b);
+    }
+
     @Test
     public void dividePositivePositive(){
         assertEquals(calculator.divide(4,2), 2);
@@ -31,24 +51,41 @@ public class DivisionTest {
     }
 
     @Test
-    public void divide_negative_positive() {
+    public void divideNegativePositive() {
         assertEquals(calculator.divide(-4, 2), -2);
     }
     @Test
-    public void divide_negative_negative() {
+    public void divideNegativeNegative() {
         assertEquals(calculator.divide(-4,-2), 2);
     }
     @Test(expected = Exception.class)
-    public void divide_positive_zero(){
+    public void dividePositiveZero(){
         calculator.divide(1,0);
     }
     @Test
-    public void divide_zero_positive() {
+    public void divideZeroPositive() {
         assertEquals(calculator.divide(0,-2), 0);
     }
     @Test(expected = Exception.class)
-    public void divide_zero_zero(){
+    public void divideZeroZero(){
         calculator.divide(0,0);
+    }
+
+    @Test
+    public void divideMaxMax(){
+        assertEquals(calculator.divide(Integer.MAX_VALUE, Integer.MAX_VALUE),1);
+    }
+    @Test
+    public void divideZeroMax(){
+        assertEquals(calculator.divide(0, Integer.MAX_VALUE), 0);
+    }
+    @Test
+    public void divideMaxNegative(){
+        assertEquals(calculator.divide( Integer.MAX_VALUE,-1), Integer.MIN_VALUE+1);
+    }
+    @Test(expected = Exception.class)
+    public void divideMaxZero(){
+        calculator.divide(Integer.MAX_VALUE,0);
     }
 
 }
